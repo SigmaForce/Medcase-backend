c3b5ecb347779b7619c0a2cf0454daee796e461f
Leonardo Matos <leonardolucas2020@gmail.com>
2026-03-10 21:42:56 -0400

    :sparkles: feat: create `current-user` decorator

---
+++ src/infra/http/decorators/current-user.decorator.ts
@@ -0,0 +1,12 @@
+import { createParamDecorator, ExecutionContext } from '@nestjs/common'
+
+export interface JwtPayload {
+  sub: string
+  email: string
+  role: string
+}
+
+export const CurrentUser = createParamDecorator((_: unknown, ctx: ExecutionContext): JwtPayload => {
+  const request = ctx.switchToHttp().getRequest()
+  return request.user as JwtPayload
+})
