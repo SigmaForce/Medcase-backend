5db664226c3f52e3009cc9e42c0e52e0500bfe69
Leonardo Matos <leonardolucas2020@gmail.com>
2026-03-10 21:34:06 -0400

    :sparkles: feat: create `email-service` interface

---
+++ src/modules/identity/domain/interfaces/email-service.interface.ts
@@ -0,0 +1,16 @@
+export interface SendEmailConfirmationParams {
+  to: string
+  token: string
+  fullName: string
+}
+
+export interface SendPasswordResetParams {
+  to: string
+  token: string
+  fullName: string
+}
+
+export interface IEmailService {
+  sendEmailConfirmation(params: SendEmailConfirmationParams): Promise<void>
+  sendPasswordReset(params: SendPasswordResetParams): Promise<void>
+}
