9d8fa7a0542049a834205999e68b6f6395107fb4
Leonardo Matos <leonardolucas2020@gmail.com>
2026-03-10 21:44:39 -0400

    :sparkles: feat: create `jwt-auth` guard

---
+++ src/infra/http/guards/jwt-auth.guard.ts
@@ -0,0 +1,44 @@
+import { CanActivate, ExecutionContext, Injectable, UnauthorizedException } from '@nestjs/common'
+import { Reflector } from '@nestjs/core'
+import { JwtService } from '@nestjs/jwt'
+import { Request } from 'express'
+import { env } from '../../../config/env'
+import { IS_PUBLIC_KEY } from '../decorators/public.decorator'
+
+@Injectable()
+export class JwtAuthGuard implements CanActivate {
+  constructor(
+    private readonly jwtService: JwtService,
+    private readonly reflector: Reflector,
+  ) {}
+
+  async canActivate(context: ExecutionContext): Promise<boolean> {
+    const isPublic = this.reflector.getAllAndOverride<boolean>(IS_PUBLIC_KEY, [
+      context.getHandler(),
+      context.getClass(),
+    ])
+
+    if (isPublic) return true
+
+    const request = context.switchToHttp().getRequest<Request>()
+    const token = this.extractBearerToken(request)
+
+    if (!token) {
+      throw new UnauthorizedException({ error: 'MISSING_TOKEN' })
+    }
+
+    try {
+      const payload = await this.jwtService.verifyAsync(token, { secret: env.JWT_SECRET })
+      request['user'] = payload
+    } catch {
+      throw new UnauthorizedException({ error: 'INVALID_TOKEN' })
+    }
+
+    return true
+  }
+
+  private extractBearerToken(request: Request): string | null {
+    const [type, token] = request.headers.authorization?.split(' ') ?? []
+    return type === 'Bearer' ? token : null
+  }
+}
