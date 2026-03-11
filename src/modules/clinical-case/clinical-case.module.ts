545a7d85aad311cb00f4ef710b085d0881594849
Leonardo Matos <leonardolucas2020@gmail.com>
2026-03-11 17:57:12 -0400

    :sparkles: feat: create clinical case module

---
+++ src/modules/clinical-case/clinical-case.module.ts
@@ -0,0 +1,37 @@
+import { Module } from '@nestjs/common'
+import { SubscriptionModule } from '../subscription/subscription.module'
+
+// Repositories
+import { PrismaClinicalCaseRepository } from './infrastructure/repositories/prisma-clinical-case.repository'
+import { PrismaSpecialtyRepository } from './infrastructure/repositories/prisma-specialty.repository'
+
+// Adapters & Services
+import { OpenAiAdapter } from './infrastructure/adapters/openai.adapter'
+import { CaseGeneratorService } from './infrastructure/services/case-generator.service'
+
+// Use Cases
+import { ListCases } from './application/use-cases/ListCases'
+import { GetCase } from './application/use-cases/GetCase'
+import { GenerateCase } from './application/use-cases/GenerateCase'
+import { ListSpecialties } from './application/use-cases/ListSpecialties'
+
+// Controllers
+import { CaseController } from './presentation/controllers/case.controller'
+import { CaseGenerateController } from './presentation/controllers/case-generate.controller'
+import { SpecialtyController } from './presentation/controllers/specialty.controller'
+
+@Module({
+  imports: [SubscriptionModule],
+  providers: [
+    { provide: 'IClinicalCaseRepository', useClass: PrismaClinicalCaseRepository },
+    { provide: 'ISpecialtyRepository', useClass: PrismaSpecialtyRepository },
+    OpenAiAdapter,
+    CaseGeneratorService,
+    ListCases,
+    GetCase,
+    GenerateCase,
+    ListSpecialties,
+  ],
+  controllers: [CaseController, CaseGenerateController, SpecialtyController],
+})
+export class ClinicalCaseModule {}
