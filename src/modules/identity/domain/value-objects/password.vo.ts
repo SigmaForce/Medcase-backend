188a5466c5b71425b9d3a9d9ab79c908486d6480
Leonardo Matos <leonardolucas2020@gmail.com>
2026-03-10 21:36:49 -0400

    :sparkles: feat: create domain `password` value object

---
+++ src/modules/identity/domain/value-objects/password.vo.ts
@@ -0,0 +1,20 @@
+import { DomainException } from '../../../../errors/domain-exception'
+
+const PASSWORD_REGEX = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*]).{8,}$/
+
+export class Password {
+  static validate(raw: string, email: string): void {
+    if (!PASSWORD_REGEX.test(raw)) {
+      throw new DomainException(
+        'PASSWORD_TOO_WEAK',
+        400,
+        'Mínimo 8 chars, 1 maiúscula, 1 minúscula, 1 número, 1 especial',
+      )
+    }
+
+    const emailUsername = email.split('@')[0].toLowerCase()
+    if (raw.toLowerCase().includes(emailUsername)) {
+      throw new DomainException('PASSWORD_TOO_WEAK', 400, 'A senha não pode conter o e-mail')
+    }
+  }
+}
