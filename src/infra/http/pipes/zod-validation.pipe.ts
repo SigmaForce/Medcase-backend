78add6428c5d87e84cc4122cc4796bb6a1dc81ec
Leonardo Matos <leonardolucas2020@gmail.com>
2026-03-10 21:44:28 -0400

    :sparkles: feat: create `zod-validation` pipe

---
+++ src/infra/http/pipes/zod-validation.pipe.ts
@@ -0,0 +1,10 @@
+import { PipeTransform } from '@nestjs/common'
+import { ZodSchema } from 'zod'
+
+export class ZodValidationPipe implements PipeTransform {
+  constructor(private readonly schema: ZodSchema) {}
+
+  transform(value: unknown) {
+    return this.schema.parse(value)
+  }
+}
